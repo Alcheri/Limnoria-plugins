173aeedf37e20006157bcbd5550c30b8486b0237
Barry Suridge <barry.suridge@gmail.com>
2025-01-07 02:11:21 +1100

    Initial init 07-01-2025

---
+++ README.md
@@ -0,0 +1,36 @@
+# URLtitle
+
+![Python versions](https://img.shields.io/badge/Python-version-blue) ![Supported Python versions](https://img.shields.io/badge/3.9%2C%203.10%2C%203.11%2C%203.12-blue.svg) ![Build Status](https://github.com/Alcheri/My-Limnoria-Plugins/blob/master/img/status.svg) ![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)
+
+# Description
+
+A simple plugin that detects URLs in a channel and returns the page title.
+
+## Install
+
+Go into your Limnoria plugin dir, usually ~/runbot/plugins and run:
+
+```plaintext
+git clone https://github.com/Alcheri/URLtitle.git
+```
+
+To install additional requirements, run from /plugins/URLtitle:
+
+```plaintext
+pip install --upgrade -r requirements.txt 
+```
+
+Next, load the plugin:
+
+```plaintext
+/msg bot load URLtitle
+```
+
+## Configuring
+
+* **config channel #channel plugins.URLtitle.enabled True or False (On or Off)**
+
+    Should plugin work in this channel?
+
+<br/><br/>
+<p align="center">Copyright © MMXXIV, Barry Suridge</p>
---
+++ plugin.py
@@ -0,0 +1,93 @@
+###
+# Copyright (c) 2025, Barry Suridge
+# All rights reserved.
+#
+#
+###
+
+import re
+import requests
+import time
+
+#XXX Third-party modules
+try:
+    from bs4 import BeautifulSoup
+except ImportError as ie:
+    raise Exception(f'Cannot import module: {ie}')
+
+from supybot import callbacks
+from supybot.commands import *
+from supybot.i18n import PluginInternationalization
+
+
+_ = PluginInternationalization('URLtitle')
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:110.0) Gecko/20100101 Firefox/110.0'
+}
+
+class URLtitle(callbacks.Plugin):
+    """
+    Automatically detects URLs in messages and replies with the website title, with caching.
+    """
+    threaded = True
+
+    def __init__(self, irc):
+        super().__init__(irc)
+        self.cache = {}  # Simple cache for storing URL titles
+
+    def fetch_title(self, url):
+        # Check the cache
+        if url in self.cache:
+            title, timestamp = self.cache[url]
+            if time.time() - timestamp < 600:  # Cache expiration logic
+                return title
+
+        try:
+            # Fetch the webpage
+            response = requests.get(url, headers=HEADERS, timeout=10)
+            response.raise_for_status()
+
+            # Parse the HTML and extract the title
+            soup = BeautifulSoup(response.text, 'html.parser')
+            title_tag = soup.find('title')
+
+            if title_tag:
+                formatted_title = f"{title_tag.get_text(strip=True)}"
+            else:
+                formatted_title = f"Title for {url}: No title found"
+
+            # Update the cache
+            self.cache[url] = (formatted_title, time.time())
+            return formatted_title
+        except Exception as e:
+            self.log.error(f"{e}")
+            return f"Error fetching {url}:"
+
+    def doPrivmsg(self, irc, msg):
+        """
+        Triggered when a message is sent in a channel.
+        """
+        channel = msg.args[0]
+        if not self.registryValue('enabled', channel, irc.network):
+            return
+        text = msg.args[1]
+
+        # Regular expression to detect URLs
+        url_pattern = r'(https?://\S+|www\.\S+)'
+        urls = re.findall(url_pattern, text)
+
+        if urls:
+            for url in urls:
+                # Add http if the URL does not include a scheme
+                if not url.startswith(('http://', 'https://')):
+                    url = 'http://' + url
+
+                title = self.fetch_title(url)
+                irc.reply(title, to=channel)
+
+
+Class = URLtitle
+
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
