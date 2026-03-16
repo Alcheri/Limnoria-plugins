b653dfc74e55f1c83c13ce13aa145e991c49e552
Alcheri <barry.suridge@gmail.com>
2026-03-16 17:56:13 +1100

    Fix test import handling and add Asyncio test scaffold

---
+++ test.py
@@ -0,0 +1,22 @@
+###
+# Copyright (c) 2026, Barry Suridge
+# All rights reserved.
+###
+
+import unittest
+
+from supybot.test import *
+
+
+class AsyncioTestCase(PluginTestCase):
+    plugins = ("Asyncio",)
+
+
+class AsyncioSmokeTestCase(unittest.TestCase):
+    def test_plugin_module_exports_class(self):
+        from . import plugin
+
+        self.assertTrue(hasattr(plugin, "Class"))
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
