2d85275b926b963c4a5826918ec135587822c813
Alcheri <barry.suridge@gmail.com>
2026-04-05 01:45:04 +1100

    Squashed 'plugins/GoogleMaps/' content from commit 9bf3bcea4
    git-subtree-dir: plugins/GoogleMaps
    git-subtree-split: 9bf3bcea4d16d7c452fb9f5487747e0be39b5c39

---
+++ config.py
@@ -0,0 +1,38 @@
+###
+# Copyright © 2024, Barry KW Suridge
+# All rights reserved.
+#
+#
+###
+
+from supybot import conf, registry
+
+try:
+    from supybot.i18n import PluginInternationalization
+
+    _ = PluginInternationalization("GoogleMaps")
+except:
+    _ = lambda x: x
+
+
+def configure(advanced):
+    from supybot.questions import expect, anything, something, yn
+
+    conf.registerPlugin("GoogleMaps", True)
+
+
+GoogleMaps = conf.registerPlugin("GoogleMaps")
+
+conf.registerGlobalValue(
+    GoogleMaps,
+    "googlemapsAPI",
+    registry.String("", _("""Sets the API key for Google Maps."""), private=True),
+)
+
+conf.registerChannelValue(
+    GoogleMaps,
+    "enabled",
+    registry.Boolean(False, """Should plugin work in this channel?"""),
+)
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
